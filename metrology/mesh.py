1e61280bcdb06b74f7c70ef82a6d01a416adde1a
ablue <andrew.blue@cern.ch>
2020-05-04 10:48:06 +0100

    mesh plotting now added to gui

--- metrology/mesh.py
+++ metrology/mesh.py
@@ -1,34 +1,19 @@
-import pandas as pd
-from pylab import *
-import numpy as np
-from matplotlib import cm
+import csv
+import sys
 import matplotlib.pyplot as plt
-from scipy.interpolate import griddata
+import numpy as np
+import pandas as pd
 
 df = pd.read_csv('tr.csv')
-x =df['x']
-y =df['y']
-z =df['z']
+X =df['x']
+Y =df['y']
+Z =df['z']
 
-# make up data.
-npts = 200
-# define grid.
-xi = np.linspace(95, 100, 100)	# 100 x grid points
-yi = np.linspace(19, 21, 200)	# 200 y grid points
 
-# mlab.griddata accepts the xi and yi 1d arrays above with different lengths.
-# scipy's griddata requires a full grid array. This is created with the mgrid function.
-xi, yi = np.mgrid[95:199:100j, 19:21:200j]
-# grid the data.
-# points = np.vstack((x,y)).T
-zi = griddata((x,y), z, (xi, yi), method='cubic')
-# contour the gridded data, plotting dots at the nonuniform data points.
-CS = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
-CS = plt.contourf(xi, yi, zi, 15)
-plt.colorbar()  # draw colorbar
-# plot data points.
-# plt.scatter(x, y, marker='o', s=5, zorder=10)
-plt.xlim(95, 199)
-plt.ylim(19, 21)
-plt.title('griddata test (%d points)' % npts)
+
+# Plot X,Y,Z
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_trisurf(X, Y, Z, color='white', edgecolors='grey', alpha=0.5)
+ax.scatter(X, Y, Z, c='red')
 plt.show()
--- test.py
+++ test.py
@@ -4,9 +4,8 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox, filedialog
 import matplotlib.pyplot as plt
-import pandas as pd
-import xlrd
 import numpy as np
+import pandas as pd
 
 master = Tk()
 master.title("Base")
@@ -25,20 +24,8 @@ def open():
 
     return
 
-def open_x():
-    global ppx
-    master.filename = filedialog.askopenfilename(title="Select a file",
-                                                 filetypes=(("xls files", "*.xls"), ("all files", "*.*")))
-    ppx = master.filename
-    # het filename from location
-    ppx_filename = os.path.basename(ppx)
-    # populate e1 with file location
-    e2.insert(0, ppx_filename)
 
-    return
-
-
-def process():
+def process_h():
     # os.system('python z_data.py ' + pp)
     df = pd.read_csv(pp)
     hist_plot = df['z'].hist(bins=100)
@@ -48,56 +35,32 @@ def process():
     hist_plot.plot()
     plt.show()
 
-def processx():
-    book = xlrd.open_workbook(ppx)
-    sheet = book.sheet_by_index(0)
-    total_rows = sheet.nrows
-    total_columns = sheet.ncols
-    colz1 = []
-    colz2 = []
-    for row in range(sheet.nrows):
-        colz1.append(sheet.cell_value(row, 3))
-    for row in range(sheet.nrows):
-        colz2.append(sheet.cell_value(row, 9))
+def process_s():
 
-    new_colz1 = [x for x in colz1 if isinstance(x, float)]
-    new_colz2 = [x for x in colz2 if isinstance(x, float)]
-    len_new_colz1 = len(colz1)
-    len_new_colz2 = len(colz2)
-    even_indexes_colz1 = range(0, len_new_colz1, 2)
-    even_indexes_colz2 = range(0, len_new_colz2, 2)
-    actual_colz1 = [e for i, e in enumerate(new_colz1) if i in even_indexes_colz1]
-    actual_colz2 = [e for i, e in enumerate(new_colz2) if i in even_indexes_colz2]
-    tot_actual_z = [None] * (len(actual_colz1) + len(actual_colz2))
-    tot_actual_z[::2] = actual_colz1
-    tot_actual_z[1::2] = actual_colz2
-    average_z = []
-    for i in range(len(tot_actual_z), 4, 5):
-        average_z.append(np.mean(tot_actual_z[i:(i + 5)]))
-    print(average_z)
-    plt.figure(1)
-    plt.plot(average_z)
-    plt.xlabel('Asic position')
-    plt.ylabel('Height in mm')
-    plt.title('Asic pad height')
+    df = pd.read_csv(pp)
+    X = df['x']
+    Y = df['y']
+    Z = df['z']
+
+    # Plot X,Y,Z
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(X, Y, Z, color='white', edgecolors='grey', alpha=0.5)
+    ax.scatter(X, Y, Z, c='red')
     plt.show()
 
 # this will create a label widget
 l1 = Label(master, text="CSV File path")
-l2 = Label(master, text="XLS File path")
 
 # grid method to arrange labels in respective
 # rows and columns as specified
 l1.grid(row=0, column=0, sticky=W, pady=2)
-l2.grid(row=2, column=0, sticky=W, pady=2)
 
 # entry widgets, used to take entry from user
 e1 = Entry(master)
-e2 = Entry(master)
 
 # this will arrange entry widgets
 e1.grid(row=0, column=1, pady=2)
-e2.grid(row=2, column=1, pady=2)
 
 # adding image (remember image should be PNG and not JPG)
 img = PhotoImage(file=r"logo.png")
@@ -109,10 +72,8 @@ Label(master, image=img1).grid(row=0, column=2,
 
 # button widget
 b1 = Button(master, text="Choose CSV File", command=open)
-b2 = Button(master, text="Process", command=process)
-b4 = Button(master, text="Choose XLS File", command=open_x)
-b5 = Button(master, text="Process", command=processx)
-
+b2 = Button(master, text="Histogram", command=process_h)
+b5 = Button(master, text="Surface Plot", command=process_s)
 b3 = Button(master, text="Quit", command=quit)
 
 
@@ -125,11 +86,10 @@ def home(self):
 
 
 # arranging button widgets
-b1.grid(row=1, column=0, sticky=E)
-b2.grid(row=1, column=1, sticky=W)
+b1.grid(row=1, column=0, columnspan=3, sticky=W)
+b2.grid(row=2, column=0, sticky=E)
 b3.grid(row=2, column=3, sticky=E)
-b4.grid(row=3, column=0, sticky=E)
-b5.grid(row=3, column=1, sticky=W)
+b5.grid(row=2, column=1, sticky=W)
 
 
 mainloop()
